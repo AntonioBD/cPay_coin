35fa076f31f001c073066a08b4dfc658f7030af9
Andriy Oblivantsev <eslider@gmail.com>
2018-05-26 13:43:06 +0100

    Disable DNS lookup by default

--- src/netbase.h
+++ src/netbase.h
@@ -23,7 +23,7 @@ extern bool fNameLookup;
 //! -timeout default
 static const int DEFAULT_CONNECT_TIMEOUT = 5000;
 //! -dns default
-static const int DEFAULT_NAME_LOOKUP = true;
+static const int DEFAULT_NAME_LOOKUP = false;
 
 class proxyType
 {
